76adb45f2e6e091413b8bdbd58c2c253686361af
bruno_ballinari <brunoballinari@gmail.com>
2020-09-07 12:48:34 -0300

    commit inicial

---
+++ tpN4.py
@@ -0,0 +1,10 @@
+#-------------------------------------------------------------------------------
+#
+# 2)Una facultad toma tres parciales a sus alumnos.Para aprobar:
+#   - El promedio debe ser mayor o igual a 6.
+#   - La nota del tercer parcial debe ser mayor o igual que 6.
+#   Hacer un programa que permita al docente ingresar las notas de los tres parci-
+#   ales de un alumno. Ademas debe mostrar el promedio del alumno e indicar si
+#   aprobo o no la materia.
+#
+#-------------------------------------------------------------------------------
